d800c927f9ce46b46c5707ce4aa92e8ab43d039a
Nuno Spencer <nmelo_6809@email.ric.edu>
2017-08-29 20:18:36 -0400

    Update Test.java

--- Documentation/Test.java
+++ Documentation/Test.java
@@ -5,6 +5,8 @@
  *
  * www.nunospencer.com
  * www.nunospencer.com/passlockv1
+ *
+ *Class to test "SEARCH" feature... after many unsucessful tries, this code finally worked
  */
 
 import java.io.File;
@@ -28,16 +30,16 @@ public class Test {
 
     private static void findPasswords(String userInput)
     {
-        File file = new File("C:\\Users\\Nuno\\Documents\\my_passwords.txt");   //file to search
+        File file = new File("C:\\Users\\Nuno\\Documents\\my_passwords.txt");   //file to be searched
         Scanner scan = new Scanner(System.in);                                  //Scanner object to get user input
         String accntToken;                                                      //first string token of a line, i.e. "account"
-        String getLine;                                                         //this is the line that contains the password the user is looking for. There may be +1 lines thus I have to return them all.*** must use ArrayList of strings (lines of strings) instead of Array
+        String getLine;                                                         //this is the line that contains the password the user is searching. There may be +1 lines thus I have to return them all collected in ArrayList (resizes as needed, contrary to Array which is fixed in size)
         ArrayList<String> collectedLines = new ArrayList<>();                   //any matched line(s) is collected to an ArrayList of strings (lines)
         
         String nextOutput;                                                      //line(s) that was collected from ArrayList
         boolean isFound = false;
-        int i;                                                                  //delineator... finds index of 1st space " " on that line  (or try ' '), so we can get to the 1st token of a line i.e. "account"
-        int v = 0;                                                              //counter... everytime an input is matched to a line, the line is collected to ArrayList... increas    e v... the pane will also display number of accounts found (i.e. v)
+        int i;                                                                  //delineator... finds index of 1st space " " on that line, so we can get to the 1st token of a line i.e. "account"
+        int v = 0;                                                              //counter... everytime an input is matched to a line, the line is collected to ArrayList, v++ ... then pane will also display number of accounts found (i.e. v)
 
         try
         {   
@@ -49,22 +51,22 @@ public class Test {
                 accntToken = getLine.substring(0, i);                           //gets the account (1st string token in the line, after " ")                        
                 if(userInput.equalsIgnoreCase(accntToken))                      //if given input account equals the account on the line 
                 {     
-                    collectedLines.add(getLine);                                //append the line to the ArrayList collectedLines
-                    isFound = true;                                             //flag
+                    collectedLines.add(getLine);                                //append the line/s to the ArrayList
+                    isFound = true;                                             //flag as found
                     v++;                                                        //increment number of matches found
                 }
             }
-            System.out.println(v + " password(s) found for \"" + userInput + "\" :"); //tests to see search algorithm is working
+            System.out.println(v + " password(s) found for \"" + userInput + "\" :"); //tests to see that search algorithm is working
             
-            if(isFound)                                                         //if passwords found, display them in my custom window
+            if(isFound)                                                         //if passwords were found, display them as following:
             {
-                ListIterator<String> outputMatches =  collectedLines.listIterator(); 
-                while(outputMatches.hasNext())
+                ListIterator<String> outputMatches =  collectedLines.listIterator(); //iterator for ArrayList...
+                while(outputMatches.hasNext())                                          //while ArrayList is not empty
                 {
-                    nextOutput = (String)outputMatches.next(); 
-                    System.out.println(nextOutput);
+                    nextOutput = (String)outputMatches.next();                          //add found passwords(lines) to the string var to be return
+                    System.out.println(nextOutput);                                     //return/print the lines
                 }
-                outputMatches.remove();
+                outputMatches.remove();                                                 //do away with the iterator
                 
             }else
             {
